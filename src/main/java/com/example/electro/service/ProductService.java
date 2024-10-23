1d8c31388fa665480d2ec54477a2d6d7cacad13d
ahmedmandour <ahmedmandor@rocketmail.com>
2024-10-23 11:49:06 +0300

    adding  Mapstruct feature
    added mapstruct feature and integrate it to map product entity to dtos and vice versa

--- src/main/java/com/example/electro/service/ProductService.java
+++ src/main/java/com/example/electro/service/ProductService.java
@@ -1,15 +1,14 @@
 package com.example.electro.service;
 
 import com.example.electro.dto.ProductDTO;
+import com.example.electro.mapper.ProductMapper;
 import com.example.electro.model.Product;
 import com.example.electro.repository.ProductRepository;
 import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -24,7 +23,7 @@ public class ProductService {
     public Product findById(Integer id){
         return productRepository.findById(id).orElseThrow(()->new RuntimeException("Product Not Found"));
     }
-    public List<Product> findAll(){
-        return productRepository.findAll();
+    public List<ProductDTO> findAll(){
+        return ProductMapper.INSTANCE.toDTOs(productRepository.findAll());
     }
 }
