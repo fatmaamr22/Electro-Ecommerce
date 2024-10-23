1071035c105a9673b413dbc454adead3956f3db7
fatmaamr22 <fatmaamro34@gmail.com>
2024-10-23 22:00:23 +0300

    Edit Order Controller and Add FrontEnd package
    Add more functions in Order Controller and Edit Order Service
    
    Add Spring Boot Servlet Initializer
    
    Add JSPs,css and all needed files for Frontend

---
+++ src/main/java/com/example/electro/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.electro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // This will map to home.jsp
+    }
+}
--- src/main/java/com/example/electro/controller/OrderController.java
+++ src/main/java/com/example/electro/controller/OrderController.java
@@ -3,28 +3,45 @@ package com.example.electro.controller;
 import com.example.electro.dto.OrderDTO;
 import com.example.electro.model.Order;
 import com.example.electro.service.OrderService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("orders")
 public class OrderController {
-    @Autowired
     private OrderService orderService;
 
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    // Review Specific Order in Admin Panel
     @GetMapping("/{id}")
     public OrderDTO reviewOrder(@PathVariable("id") int id) {
         return orderService.getOrderById(id);
     }
 
+    // Review All orders in Admin Panel
     @GetMapping
-    public List<OrderDTO> getAllOrders() {
-        return orderService.getAllOrders(3,3);
+    public List<OrderDTO> getAllOrders(@RequestParam(value = "page") Integer pageNumber) {
+        return orderService.getAllOrders(pageNumber,10);
+    }
+
+    // Review Order History Of Customer
+    @GetMapping("/customers/{id}")
+    public List<OrderDTO> getOrdersByCustomerId(@PathVariable("id") int customerId, @RequestParam(value = "page") Integer pageNumber) {
+        return orderService.getOrdersbyCustomerId(customerId,pageNumber,10);
+    }
+
+    // Review All Orders Using A Specific Coupon in Admin Panel
+    @GetMapping("/coupons/{name}")
+    public List<OrderDTO> getOrdersByCoupon(@PathVariable("name") String couponName) {
+        return orderService.getOrdersbyCouponName(couponName);
     }
 
 }
