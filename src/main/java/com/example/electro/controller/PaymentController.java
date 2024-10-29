5a200841fc044ed62319918d1f5499e66139dd89
fatmaamr22 <fatmaamro34@gmail.com>
2024-10-30 00:37:33 +0300

    Edit Payment Controller

--- src/main/java/com/example/electro/controller/PaymentController.java
+++ src/main/java/com/example/electro/controller/PaymentController.java
@@ -4,9 +4,11 @@ import com.example.electro.enums.OrderState;
 import com.example.electro.service.OrderService;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("payment")
 public class PaymentController {
 
@@ -17,7 +19,7 @@ public class PaymentController {
     }
 
     @PostMapping
-    public void handlePaymentCallback(@RequestBody String Payload){
+    public String handlePaymentCallback(@RequestBody String Payload, Model model) {
         JsonObject jsonObject = JsonParser.parseString(Payload).getAsJsonObject();
         boolean state = jsonObject.getAsJsonObject("obj").getAsJsonPrimitive("success").getAsBoolean();
 
@@ -29,8 +31,12 @@ public class PaymentController {
 
         if(state){
             orderService.updateOrderState(orderId, OrderState.PROCESSING);
+            return "index";
         } else {
             orderService.updateOrderState(orderId, OrderState.CANCELLED);
+            model.addAttribute("message", "Payment Failed");
+            model.addAttribute("status", 503);
+            return "error";
         }
     }
 
